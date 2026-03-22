9dc6711e92d2122e6189f0cf306c9fc8a500349b
Aniruddha Roy <ar.roy564@gmail.com>
2026-03-23 03:12:27 +0600

    backend package update

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,15 +1,15 @@
 import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in the .env file");
 }
 
-export default defineConfig({
+export default {
   out: "./drizzle",
   schema: "./src/db/models/index.ts",
   dialect: "mysql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-});
+} satisfies Config;
